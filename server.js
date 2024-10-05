540217ea8d5f5a00418e900454a7928e0bb154bf
Larry Olguin <iraqwarvet31@gmail.com>
2024-10-05 12:35:59 -0700

    add additional route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/api/data', (req, res) => {
+  res.send('THIS WILL BE YOUR SAMPLE DATA RETURNED YAY!!');
+});
+
 app.listen(port, () => {
   console.log(`Node backend app running on port: ${port}`);
 });
